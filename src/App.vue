d652a67a4d29d29904e8a8dae341630f1b3b757b
adhampton110 <anthony.hampton1235@gmail.com>
2023-07-20 01:14:18 -0500

    Header + Hero Content

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <img src="./assets/logo.svg" alt="logo">
+    <div class="links">
+      <a href="#">Home</a>
+      <a href="#">What is GPT?</a>
+      <a href="#">Open AI</a>
+      <a href="#">Case Studies</a>
+      <a href="#">Library</a>
+    </div>
+    <div class="login">
+      <a href="#">Sign in</a>
+      <button>Sign up</button>
+    </div>
+  </header>
+  <section class="hero">
+    <div class="content">
+      <h1>Let's Build Something amazing with GPT-3 OpenAI</h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+      </p>
+      <div class="getting-started">
+        <input placeholder="Your Email Address" type="text">
+        <button>Get Started</button>
+      </div>
+    </div>
+    <img src="./assets/ai.png" width="670px" height="700px" alt="ai">
+  </section>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
